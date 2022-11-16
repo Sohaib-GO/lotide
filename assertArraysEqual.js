54eb2f756f7190ff88a2d26df358c4c9f7162c60
Sohaib-GO <s.almajmaie@gmail.com>
2022-11-15 20:02:03 -0700

    Added a function which will take in two arrays and console.log an appropriate message to the console

---
+++ assertArraysEqual.js
@@ -0,0 +1,15 @@
+const eqArrays = require('./eqArrays');
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+// TEST CODE
+// assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+// assertArraysEqual([1, 2, 3], [3, 2, 1]); // => should FAIL
+// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => should PASS
+
