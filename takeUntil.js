3e8a2f5125ef35eb5d1a33da4aaa40e7d8f96294
Sohaib-GO <s.almajmaie@gmail.com>
2022-11-17 16:31:42 -0700

    Added TEST CODE

--- takeUntil.js
+++ takeUntil.js
@@ -25,3 +25,10 @@ const data2 = [
 ];
 const results2 = takeUntil(data2, (x) => x === ",");
 console.log(results2);
+
+
+// TEST CODE
+assertArraysEqual(results1, [1, 2, 5, 7, 2]); // => should PASS
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]); // => should PASS
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood", "I've"]); // => should FAIL
+
