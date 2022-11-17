bb825009c4e7e0173dcd26c10bca9b3db2d34a5d
Sohaib-GO <s.almajmaie@gmail.com>
2022-11-16 18:07:37 -0700

    Created a function which will take in two objects and returns true or false, based on a perfect match

---
+++ eqObjects.js
@@ -0,0 +1,42 @@
+const assertEqual = require("./assertEqual");
+const eqArrays = require("./eqArrays");
+
+const eqObjects = (object1, object2) => {
+  // check if both objects have the same number of keys
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+
+  // loop over keys in object1
+  for (let key in object1) {
+    // check if key exists in object1 and object2
+    if (object1.hasOwnProperty(key) && object2.hasOwnProperty(key)) {
+      // check if value is an array
+      if (Array.isArray(object1[key])) {
+        // check if arrays are equal
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      } else {
+        // check if values are equal
+        if (object1[key] !== object2[key]) {
+          return false;
+        }
+      }
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
+
+// TEST CODE
+console.log(
+  assertEqual(eqObjects({ a: "1", b: "2" }, { b: "2", a: "1" }), true)
+); // => PASS
+console.log(
+  assertEqual(eqObjects({ a: "1", b: "2" }, { b: "2", a: "1" }), true)
+); // => PASS
+console.log(
+  assertEqual(eqObjects({ a: "1", b: "2" }, { b: "2", a: "1" }), false)
+); // => FAIL
