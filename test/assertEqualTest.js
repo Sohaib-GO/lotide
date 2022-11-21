961364e36dfc256e01f426431d98c7724427ba9c
Sohaib-GO <s.almajmaie@gmail.com>
2022-11-21 16:08:34 -0700

    Test for assertEqual function

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,4 +1,11 @@
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-assertEqual("Lighthouse Labs", "Bootcamp"); // => should fail
-assertEqual(1, 1); // => should pass
+describe("#assertEqual", () => {
+  it("returns false for 'Lighthouse Labs' and 'Bootcamp'", () => {
+    assert.strictEqual(assertEqual("Lighthouse Labs", "Bootcamp"));
+  });
+  it("returns true for 1 and 1", () => {
+    assert.strictEqual(assertEqual(1, 1));
+  });
+});
