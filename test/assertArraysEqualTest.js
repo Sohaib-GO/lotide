3d38955748b04c9447ba3d6941c9d610d8d857da
Sohaib-GO <s.almajmaie@gmail.com>
2022-11-21 16:24:34 -0700

    Added Test for assertArraysEqual function

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,11 @@
 const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => should FAIL
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => should PASS
+describe("#assertArraysEqual", () => {
+  it("returns true for [1, 2, 3] and [1, 2, 3]", () => {
+    assert.strictEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+  });
+  it("returns false for [1, 2, 3] and [3, 2, 1]", () => {
+    assert.strictEqual(assertArraysEqual([1, 2, 3], [3, 2, 1]));
+  });
+});
