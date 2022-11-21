66b1d16b962db810e1158f97a714a1110e95e36f
Sohaib-GO <s.almajmaie@gmail.com>
2022-11-21 13:59:27 -0700

    Added test for assertArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -8,9 +8,5 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-// TEST CODE
-// assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-// assertArraysEqual([1, 2, 3], [3, 2, 1]); // => should FAIL
-// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => should PASS
 
 module.exports = assertArraysEqual;
