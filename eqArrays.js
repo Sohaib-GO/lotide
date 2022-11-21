8b546f0ea7d081fae06bc5bac6e3d311ee3f0d96
Sohaib-GO <s.almajmaie@gmail.com>
2022-11-21 13:54:46 -0700

    Added test to eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -13,10 +13,5 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-// TEST CODE
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
 
 module.exports = eqArrays;
